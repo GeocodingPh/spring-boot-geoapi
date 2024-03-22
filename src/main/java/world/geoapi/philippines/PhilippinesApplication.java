fae731d3aa961624f84a4c0a9f6860d5894872b5
111coding <jwlee1024@gmail.com>
2024-03-22 23:10:28 +0900

    initial commit

---
+++ src/main/java/world/geoapi/philippines/PhilippinesApplication.java
@@ -0,0 +1,13 @@
+package world.geoapi.philippines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilippinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilippinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/world/geoapi/philippines/PhilippinesApplicationTests.java
@@ -0,0 +1,13 @@
+package world.geoapi.philippines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhilippinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
